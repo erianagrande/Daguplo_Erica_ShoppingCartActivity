d167146cde19457ed5ffe9150a76f92bd1daaac1
eri <ericamaedaguplo@gmail.com>
2026-04-16 01:44:48 +0800

    Built Product class and connected it to the main program to display product list. I also Implemented initial user input for selecting products and quantity.
    I had encountered an issue with peso sign displaying as "?", which was resolved by setting console encoding to UTF-8. I have used AI as a guide for debugging.

--- Product.cs
+++ Product.cs
@@ -6,6 +6,46 @@ namespace Daguplo_Erica_ShoppingCartActivity
 {
     internal class Product
     {
-        // magsulat ng anything
+        // Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int RemainingStock { get; set; }
+
+        // Constructor
+        public Product(int id, string name, double price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            RemainingStock = stock;
+        }
+
+        // Display product details
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id}. {Name} - \u20B1{Price} (Stock: {RemainingStock})");
+        } 
+
+        // Check if the stock is enough
+        public bool HasEnoughStock(int quantity)
+        {
+            return quantity <= RemainingStock;
+        }
+
+        // Deduct Stock
+        public void DeductStock(int quantity)
+        {
+            RemainingStock -= quantity;
+        }
+
+        // Compute total price for item 
+        public double GetItemTotal(int quantity)
+        {
+            return Price * quantity;
+        }
+
+       
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Daguplo_Erica_ShoppingCartActivity;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Product[] products = new Product[]
+{ 
+    new Product(1, "Peptide Lip Care", 250, 10),
+    new Product(1, "Lipliner", 199, 10),
+    new Product(1, "Liquid Blush", 225, 10),
+    new Product(1, "Waterproof Mascara", 199, 10),
+    new Product(1, "Eyeshadow Palette", 300, 10),
+    new Product(1, "Powder Blush", 245, 10),
+};
+
+Console.WriteLine(" Welcome to Eri the Label!");
+Console.WriteLine("---------------------------");
+
+foreach (Product p in products)
+{
+    p.DisplayProduct();
+}
+
+    
